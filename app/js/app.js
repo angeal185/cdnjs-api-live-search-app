79f264429eae80179f12c20a2a8ded123e0c5307
angeal185 <beneaves01@hotmail.com>
2017-06-21 12:45:27 +0800

    update

--- app/js/app.js
+++ app/js/app.js
@@ -11,9 +11,9 @@
       count = 1;
       $.getJSON('//api.cdnjs.com/libraries?fields=description,version,homepage,keywords,license', function(data) {
         $.each(data.results, function(key, val) {
-          if (val.name.search(myExp) != -1) {
-            output += '<ul class="collection with-header"><li class="collection-header"><h4>' + val.name + '</h4></li><li class="collection-item">Description: <span class="searchLi">' + val.description + '</span></li><li class="collection-item">Version: <span class="searchLi">' + val.version + '</span></li><li class="collection-item">Full-List: <a href="https://cdnjs.com/libraries/' + val.name + '" class="searchLi" target="_blank">https://cdnjs.com/libraries/' + val.name + '</a></li><li class="collection-item">Quick-Link: <a href="' + val.latest + '" download class="searchLi" target="_blank">' + val.latest + '</a></li><li class="collection-item">Homepage: <a href="' + val.homepage + '" class="searchLi" target="_blank">' + val.homepage + '</a></li><li class="collection-item">License: <span class="searchLi">' + val.license + '</span></li><li class="collection-item">Keywords: <span class="searchLi">' + val.keywords + '</span></li></ul>';
-            if (count % 2 == 0) {
+          if (val.name.search(myExp) !== -1) {
+            output += '<ul class="collection with-header"><li class="collection-header"><h4>' + val.name + '</h4></li><li class="collection-item">Description: <span class="searchLi">' + val.description + '</span></li><li class="collection-item">Version: <span class="searchLi">' + val.version + '</span></li><li class="collection-item">Full-List: <a href="https://cdnjs.com/libraries/' + val.name + '" class="searchLi" target="_blank">https://cdnjs.com/libraries/' + val.name + '</a></li><li class="collection-item">Quick-Link: <a href="' + val.latest + '" download class="searchLi" target="_blank">' + val.latest + '</a></li><li class="collection-item">Homepage: <a href="' + val.homepage + '" class="searchLi" target="_blank">' + val.homepage + '</a></li><li class="collection-item">License: <span class="searchLi">' + val.license + '</span></li><li class="collection-item">Keywords: <span class="searchLi truncate">' + val.keywords + '</span></li></ul>';
+            if (count % 2 === 0) {
               output += '</div><div class="row">';
             }
             count++;
